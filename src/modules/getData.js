ae37a30766b86fbf8864619d2859f5afd70e33fb
Gre7 <53835219+Gre7@users.noreply.github.com>
2020-06-13 23:41:14 +0300

    Add files via upload

---
+++ src/modules/getData.js
@@ -0,0 +1,18 @@
+export default function getData() {
+	const goodsWrapper = document.querySelector('.goods');
+	return fetch('../db/db.json')
+	  .then((response) => {
+		if (response.ok) {
+		   return response.json();
+		}  else {
+		   throw new Error ('No data was received:' + response.status);
+		} 
+	})
+	.then((data) => {
+		return data
+	})
+	.catch((err) => {
+		 console.warn(err);
+		 goodsWrapper.innerHTML = '<div>Oops something went wrong!</div>'
+	});
+}
